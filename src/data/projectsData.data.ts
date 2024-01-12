b9d6fd978e0584eb51f42ce380da161254af0bb1
Andrii Horlov <110767790+FidgHorlov@users.noreply.github.com>
2024-01-12 12:58:45 +0300

    Feature/add gallery images (#7)
    * Add gallery images
    
    * Add more gallery images
    
    * Change technology stack of some projects
    
    * Add new Project (MoviePass)
    
    * Fix link, fix spaces
    
    * Fix error

--- src/data/projectsData.data.ts
+++ src/data/projectsData.data.ts
@@ -5,51 +5,97 @@ export const useProjectData = (
 ) => {
   const PROJECTS_DATA = [
     {
-      name: "VR Gallery",
+      name: "Simple Gallery VR",
       technologies: [
-        "Unity", "C#", "VR", "MetaSDK", "Work with file-system"
+        "Unity", "C#", "VR", "MetaSDK", "File System Management", "DoTween"
       ],
       desc: t("project.description_vr_gallery"),
       codeLink: "https://github.com/FidgHorlov/VrGallery",
-	    moreInfo: "",
-      demoLink: "https://github.com/FidgHorlov/VrGallery/releases/tag/release",
+	  moreInfo: "",
+      demoLink: "https://github.com/FidgHorlov/VrGallery/releases",
       gallery: [
-        'VR_Gallery/item1.jpeg',
-        'VR_Gallery/item2.jpeg',
-        'VR_Gallery/item3.jpeg',
-        'VR_Gallery/item4.jpeg',
-        'VR_Gallery/item5.jpeg',
+        'GalleryVR/GalleryVR_1.jpg',
+        'GalleryVR/GalleryVR_2.jpg',
+        'GalleryVR/GalleryVR_3.jpg',
+        'GalleryVR/GalleryVR_4.jpg',
+        'GalleryVR/GalleryVR_5.jpg',
+        'GalleryVR/GalleryVR_6.jpg',
+        'GalleryVR/GalleryVR_7.jpg',
+      ]
+    },
+	{
+      name: "MoviePass VR",
+      technologies: [
+        "Unity", "C#", "VR", "XR Toolkit", "API Integration", "DoTween"
+      ],
+      desc: t("project.description_moviepass_vr"),
+      codeLink: "",
+	  moreInfo: "https://tsukat.com/portfolio/moviepass-vr-cinema-app",
+      demoLink: "",
+      gallery: [
+        'MoviePassVR/MoviePassVR_1.jpg',
+        'MoviePassVR/MoviePassVR_2.jpg',
+        'MoviePassVR/MoviePassVR_3.jpg',
+        'MoviePassVR/MoviePassVR_4.jpg',
       ]
     },
     {
       name: "VR Robots",
       technologies: [
-        "Unity", "C#", "VR", "Hand-tracking", "OVR (Oculus Integration)"
+        "Unity", "C#", "VR", "Hand-tracking", "OVR (Oculus Integration)", "DoTween"
       ],
       desc: t("project.description_robots_vr"),
       codeLink: "",
-	    moreInfo : "https://tsukat.com/portfolio/koerber-vr-robots",
+	  moreInfo : "https://tsukat.com/portfolio/koerber-vr-robots",
       demoLink: "",
+	  gallery: [
+        'RobotsVR/RobotsVR_1.jpg',
+        'RobotsVR/RobotsVR_2.jpg',
+        'RobotsVR/RobotsVR_3.jpg',
+        'RobotsVR/RobotsVR_4.jpg',
+        'RobotsVR/RobotsVR_5.jpg',
+        'RobotsVR/RobotsVR_6.jpg',
+        'RobotsVR/RobotsVR_7.jpg',
+        'RobotsVR/RobotsVR_8.jpg',
+        'RobotsVR/RobotsVR_9.jpg',
+      ]
     },
     {
       name: "Learning Platform",
       technologies: [
-        "Unity", "C#", "3D", "WebGL", "Unity Customization", "API integration"
+        "Unity", "C#", "3D", "WebGL", "Unity Customization", "API integration", "Cinemamachine Brain"
       ],
       desc: t("project.description_digital_academy"),
       codeLink: "",
-	    moreInfo: "https://tsukat.com/portfolio/koerber-digital-academy",
+	  moreInfo: "https://tsukat.com/portfolio/koerber-digital-academy",
       demoLink: "",
+	  gallery: [
+        'DigitalAcademy/DigitalAcademy_1.jpg',
+        'DigitalAcademy/DigitalAcademy_2.jpg',
+        'DigitalAcademy/DigitalAcademy_3.jpg',
+        'DigitalAcademy/DigitalAcademy_4.jpg',
+        'DigitalAcademy/DigitalAcademy_5.jpg',
+        'DigitalAcademy/DigitalAcademy_6.jpg',
+      ]
     },
     {
       name: "Clerk Shadow",
       technologies: [
-        "Unity", "C#", "2D", "Windows", "WebGL"
+        "Unity", "C#", "2D", "Windows", "WebGL", "DoTween"
       ],
       desc: t("project.description_clerk_shadow"),
       codeLink: "",
-	    moreInfo: "",
+	  moreInfo: "",
       demoLink: "",
+	  gallery: [
+        'ClerkShadow/Clerk_1.jpg',
+        'ClerkShadow/Clerk_2.jpg',
+        'ClerkShadow/Clerk_3.jpg',
+        'ClerkShadow/Clerk_4.jpg',
+        'ClerkShadow/Clerk_5.jpg',
+        'ClerkShadow/Clerk_6.jpg',
+        'ClerkShadow/Clerk_7.jpg',
+      ]
     },
     {
       name: "Don’t miss your travel",
@@ -58,8 +104,18 @@ export const useProjectData = (
       ],
       desc: t("project.description_dont_miss_your_travel"),
       codeLink: "",
-	    moreInfo: "",
-      demoLink: "",
+	  moreInfo: "",
+      demoLink: "https://fidgetik.itch.io/dont-miss-your-travel",
+	  gallery: [
+        'DontMissTravel/DontMissTravel_1.jpg',
+        'DontMissTravel/DontMissTravel_2.jpg',
+        'DontMissTravel/DontMissTravel_3.jpg',
+        'DontMissTravel/DontMissTravel_4.jpg',
+        'DontMissTravel/DontMissTravel_5.jpg',
+        'DontMissTravel/DontMissTravel_6.jpg',
+        'DontMissTravel/DontMissTravel_7.jpg',
+        'DontMissTravel/DontMissTravel_8.jpg',
+      ]
     }
   ];
 
